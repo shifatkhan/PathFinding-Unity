89ca15e3ae51befd8ca403461f34aa3266c749c5
Shifat Khan <shifatkhan6@gmail.com>
2021-03-12 22:37:52 -0500

    Add basic A* pathfinding

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : IHeapItem<Node>
+{
+    public bool walkable;
+    public Vector3 worldPosition;
+
+    public int gridX;
+    public int gridY;
+
+    public int gCost; // Traversed cost - distance from start node.
+    public int hCost; // Heuristic cost - distance from end node.
+
+    public Node parent;
+
+    int heapIndex;
+
+    public Node(bool walkable, Vector3 worldPosition, int gridX, int gridY)
+    {
+        this.walkable = walkable;
+        this.worldPosition = worldPosition;
+        this.gridX = gridX;
+        this.gridY = gridY;
+    }
+
+    /// <summary>
+    /// Get the total cost , f cost
+    /// </summary>
+    /// <returns></returns>
+    public int fCost
+    {
+        get { return gCost + hCost; }
+    }
+
+    public int HeapIndex
+    {
+        get => heapIndex;
+        set => heapIndex = value;
+    }
+
+    public int CompareTo(Node nodeToCompare)
+    {
+        int compare = fCost.CompareTo(nodeToCompare.fCost);
+
+        if(compare == 0)
+        {
+            compare = hCost.CompareTo(nodeToCompare.hCost);
+        }
+
+        return -compare;
+    }
+}
---
+++ Assets/Scripts/Pathfinding.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class Pathfinding : MonoBehaviour
+{
+    public Transform seeker, target;
+
+    Grid grid;
+
+    private void Awake()
+    {
+        grid = GetComponent<Grid>();
+    }
+
+    private void Update()
+    {
+        FindPathAstar(seeker.position, target.position);
+    }
+
+    void FindPathAstar(Vector3 startPos, Vector3 targetPos)
+    {
+        // DEBUG
+        //Stopwatch sw = new Stopwatch();
+        //sw.Start();
+
+        Node startNode = grid.NodeFromWorldPoint(startPos);
+        Node targetNode = grid.NodeFromWorldPoint(targetPos);
+
+        Heap<Node> openSet = new Heap<Node>(grid.MaxSize);
+        HashSet<Node> closedSet = new HashSet<Node>();
+        openSet.Add(startNode);
+
+        while (openSet.Count > 0)
+        {
+            Node currentNode = openSet.RemoveFirst();
+            closedSet.Add(currentNode);
+
+            // Check if we reached goal.
+            if (currentNode == targetNode)
+            {
+                // DEBUG
+                //sw.Stop();
+                //print($"Path found: {sw.ElapsedMilliseconds} ms");
+
+                RetracePath(startNode, targetNode);
+                return;
+            }
+
+            // Check if neighbour is walkable or not.
+            foreach(Node neighbour in grid.GetNeighbours(currentNode))
+            {
+                if (!neighbour.walkable || closedSet.Contains(neighbour))
+                    continue;
+
+                int newCostToNeighbour = currentNode.gCost + GetDistance(currentNode, neighbour);
+
+                if(newCostToNeighbour < neighbour.gCost || !openSet.Contains(neighbour))
+                {
+                    neighbour.gCost = newCostToNeighbour;
+                    neighbour.hCost = GetDistance(neighbour, targetNode);
+                    neighbour.parent = currentNode; // To retrace path.
+
+                    if (!openSet.Contains(neighbour))
+                        openSet.Add(neighbour);
+                }
+            }
+        }
+    }
+
+    private void RetracePath(Node startNode, Node endNode)
+    {
+        List<Node> path = new List<Node>();
+
+        Node currentNode = endNode;
+        while (currentNode != startNode)
+        {
+            path.Add(currentNode);
+            currentNode = currentNode.parent;
+        }
+
+        // Reverse path to be from start to end.
+        path.Reverse();
+
+        grid.path = path;
+    }
+
+    /// <summary>
+    /// We set diagonal nodes to have a distance of 14, and perpendicular nodes to be 10.
+    /// So we end up calculating the distance with: 14y + 10(x - y)
+    /// </summary>
+    /// <param name="nodeA">Origin node</param>
+    /// <param name="nodeB">Target node</param>
+    /// <returns>Distance between nodes.</returns>
+    private int GetDistance(Node nodeA, Node nodeB)
+    {
+        int dstX = Mathf.Abs(nodeA.gridX - nodeB.gridX);
+        int dstY = Mathf.Abs(nodeA.gridY - nodeB.gridY);
+
+        if (dstX > dstY)
+            return 14 * dstY + 10 * (dstX - dstY);
+
+        return 14 * dstX + 10 * (dstY - dstX);
+    }
+}
